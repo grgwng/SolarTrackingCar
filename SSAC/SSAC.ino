905d97a74c24bf480b59455834da78727c361d3f
Cheney Cai <cheneycai2004@hotmail.com>
2022-11-03 09:55:14 -0400

    com

--- SSAC/SSAC.ino
+++ SSAC/SSAC.ino
@@ -4,6 +4,12 @@
 //digitalWrite(pin, value)
 //value: HIGH or LOW.
 
+//4 x sun sensors
+//1 x depth sensor
+
+//motor (L) pin D13
+//motor (R) pin D2
+
 void setup() {
   // put your setup code here, to run once:
 
