e3237cc11fe174c497bf073a420c4ef0574a681f
Cheney Cai <cheneycai2004@hotmail.com>
2022-11-11 20:33:30 -0500

    functions

--- SSAC/SSAC.ino
+++ SSAC/SSAC.ino
@@ -44,138 +44,15 @@ void setup() {
 
 }
 
-void drive (int power) { //power takes value between 1-100 (inclusive)
-
-  if (power > 100 || power < 1) {
-    Serial.println("power out of bounds in DRIVE");
-    abort();
-  }
-
-  int analogValue = 255 * ((float) 100.0 / power);
-
-  analogWrite(backmotorpin1, analogValue);
-  digitalWrite(backmotorpin2, LOW);
-
-}
-
-void reverse (int power) {
-
-  if (power > 100 || power < 1) {
-    Serial.println("power out of bounds in REVERSE");
-    abort();
-  }
-
-  int analogValue = 255 * ((float) 100.0 / power);
-
-  digitalWrite(backmotorpin1, LOW);
-  analogWrite(backmotorpin2, analogValue);
-
-}
-
-void stop() {
-
-  digitalWrite(backmotorpin1, LOW);
-  digitalWrite(backmotorpin2, LOW);
-
-  digitalWrite(frontmotorpin1, LOW);
-  digitalWrite(frontmotorpin2, LOW);
-
-}
-
-
-
-void left () {
-
-  digitalWrite(frontmotorpin1, HIGH);
-  digitalWrite(frontmotorpin2, LOW);
-
-}
-
-void right () {
-
-  digitalWrite(frontmotorpin1, LOW);
-  digitalWrite(frontmotorpin2, HIGH);
-
-}
-
-void stopTurn() {
-
-  digitalWrite(frontmotorpin1, LOW);
-  digitalWrite(frontmotorpin2, LOW);
-
-}
-
-//returns distance to object in cm
-int distanceFront () {
-
-  //NOTE: When object is too close or it doesn't detect anything,
-  //this function returns 1177 (by experimentation)
-
-  int distance = 0;
-  long duration = 0;
-
-  // Clears the trigPin condition
-  digitalWrite(sensor1trig, LOW);
-  delayMicroseconds(2);
-
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(sensor1trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(sensor1trig, LOW);
-  
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(sensor1echo, HIGH);
-
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (to and back)
-
-  //FOR DEBUGGING PURPOSES
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  
-  return distance;
-
-}
-
-// For the sensor reading fluctuation (1177)
-int distanceFiltered(){
-
-  int x = distanceFront();
-  if(x > 1000){
-    return pastReading;
-
-  } else {
-    pastReading = x;
-    return pastReading;
-
-}
-
-/*
-  //Calculate average over 5 most recent scans
-  int dist = distanceFront();
-
-  int sum = 0;
-  for(int i = 0; i <= READINGNUM-2; i++){
-    distReadings[i] = distReadings[i+1];
-    sum+=distReadings[i];
-  }
-  distReadings[READINGNUM-1] = dist;
-  sum+= dist;
-
-  sum /= READINGNUM;
-  
-  Serial.print("Avg Distance: ");
-  Serial.print(sum);
-  Serial.println(" cm");
-  */
-
-  // return sum;
-
-}
-
-
+//function promises
+void drive (int power);
+void reverse (int power);
+void stop(); //stops turning and cuts power to motor
+void left();
+void right();
+void stopTurn(); //cuts power to turning motor
+void distanceFront();
+void distanceFiltered();
 
 void loop() {
   
@@ -286,12 +163,134 @@ delay (500);
   }
   */
 
+}
 
+//funny functions :)
+void drive (int power) { //power takes value between 1-100 (inclusive)
 
+  if (power > 100 || power < 1) {
+    Serial.println("power out of bounds in DRIVE");
+    abort();
+  }
 
+  int analogValue = 255 * ((float) 100.0 / power);
 
+  analogWrite(backmotorpin1, analogValue);
+  digitalWrite(backmotorpin2, LOW);
 
+}
 
+void reverse (int power) {
 
+  if (power > 100 || power < 1) {
+    Serial.println("power out of bounds in REVERSE");
+    abort();
+  }
+
+  int analogValue = 255 * ((float) 100.0 / power);
+
+  digitalWrite(backmotorpin1, LOW);
+  analogWrite(backmotorpin2, analogValue);
+
+}
+
+void stop() {
+
+  digitalWrite(backmotorpin1, LOW);
+  digitalWrite(backmotorpin2, LOW);
+
+  digitalWrite(frontmotorpin1, LOW);
+  digitalWrite(frontmotorpin2, LOW);
+
+}
+
+void left () {
+
+  digitalWrite(frontmotorpin1, HIGH);
+  digitalWrite(frontmotorpin2, LOW);
+
+}
+
+void right () {
+
+  digitalWrite(frontmotorpin1, LOW);
+  digitalWrite(frontmotorpin2, HIGH);
+
+}
+
+void stopTurn() {
+
+  digitalWrite(frontmotorpin1, LOW);
+  digitalWrite(frontmotorpin2, LOW);
+
+}
+
+//returns distance to object in cm
+int distanceFront () {
+
+  //NOTE: When object is too close or it doesn't detect anything,
+  //this function returns 1177 (by experimentation)
+
+  int distance = 0;
+  long duration = 0;
+
+  // Clears the trigPin condition
+  digitalWrite(sensor1trig, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(sensor1trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensor1trig, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(sensor1echo, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (to and back)
+
+  //FOR DEBUGGING PURPOSES
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  
+  return distance;
+
+}
+
+// For the sensor reading fluctuation (1177)
+int distanceFiltered(){
+
+  int x = distanceFront();
+  if(x > 1000){
+    return pastReading;
+
+  } else {
+    pastReading = x;
+    return pastReading;
+
+}
+
+/*
+  //Calculate average over 5 most recent scans
+  int dist = distanceFront();
+
+  int sum = 0;
+  for(int i = 0; i <= READINGNUM-2; i++){
+    distReadings[i] = distReadings[i+1];
+    sum+=distReadings[i];
+  }
+  distReadings[READINGNUM-1] = dist;
+  sum+= dist;
+
+  sum /= READINGNUM;
+  
+  Serial.print("Avg Distance: ");
+  Serial.print(sum);
+  Serial.println(" cm");
+  */
+
+  // return sum;
 
 }
